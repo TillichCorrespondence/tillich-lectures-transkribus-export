61337dd43afb89b6a92cf0dc4dfb86030dba6e2f
csae8092 <p.andorfer@gmail.com>
2024-10-14 11:17:26 +0200

    add postprocess.py; closes #3 and closes #1

--- postprocess.py
+++ postprocess.py
@@ -6,6 +6,7 @@ import lxml.etree as ET
 from jinja2 import Environment, FileSystemLoader
 from tqdm import tqdm
 from acdh_tei_pyutils.tei import TeiReader
+from acdh_tei_pyutils.utils import nsmap
 
 environment = Environment(loader=FileSystemLoader("./"))
 template = environment.get_template("template.j2")
@@ -17,14 +18,17 @@ os.makedirs(editions, exist_ok=True)
 
 col_id = "271480"
 files = sorted(glob.glob("./tei/*.xml"))
-for x in files:
+for part, x in enumerate(files, start=1):
     tei_name = os.path.split(x)[-1]
     mets_name = f'{os.path.join("mets", col_id, tei_name.replace(".xml", "_image_name.xml"))}'
     mets = TeiReader(mets_name)
     img_lookup = [x.text for x in mets.any_xpath(".//item")]
     doc = TeiReader(x)
     for i, y in enumerate(doc.any_xpath(".//tei:pb")):
-        y.attrib["n"] = f"{tei_name.replace(".xml", "__")}{img_lookup[i]}"
+        y.attrib["n"] = f"part{part:01}_{img_lookup[i]}"
+    for i, y in enumerate(doc.any_xpath(".//tei:surface[@xml:id]")):
+        graphic = y.xpath(".//tei:graphic", namespaces=nsmap)[0]
+        graphic.attrib["url"] = f"part{part:01}_{img_lookup[i]}"
     doc.tree_to_file(x)
 
 for i, x in enumerate(tqdm(files)):
@@ -35,7 +39,7 @@ for i, x in enumerate(tqdm(files)):
     for y in doc.any_xpath(".//tei:surface[@xml:id]"):
         facs_id = y.attrib["{http://www.w3.org/XML/1998/namespace}id"]
         img_url = y.xpath("./tei:graphic/@url", namespaces=nsmap)[0]
-        img_id = img_url.split("_")[-1].replace(".jpg", "")
+        img_id = img_url.replace(".jpg", "")
         pb_node = doc.any_xpath(f'.//tei:pb[@facs="#{facs_id}"]')[0]
         xpath_expr = f'.//tei:ab[@facs="{facs_id}" or starts-with(@facs, "#{facs_id}_")]'
         ab_node = doc.any_xpath(xpath_expr)
@@ -77,3 +81,17 @@ for x in tqdm(files):
     pb = doc.any_xpath(".//tei:pb")[0]
     pb.attrib["corresp"] = f"{arche_base}{facs_url}"
     doc.tree_to_file(x)
+
+
+files = sorted(glob.glob('./data/editions/*xml'))
+for i, x in enumerate(files, start=1):
+    doc = TeiReader(x)
+    cur_id = f"TL-{i:04}"
+    f_name = f"{cur_id}.xml"
+    graphic = doc.any_xpath(".//tei:graphic")[0]
+    graphic.attrib["url"] = f"{cur_id}.jpg"
+
+    pb = doc.any_xpath(".//tei:pb")[0]
+    pb.attrib["facs"] = f"#{cur_id}.jpg"
+    doc.tree_to_file(os.path.join("data", "editions", f_name))
+    os.remove(x)
