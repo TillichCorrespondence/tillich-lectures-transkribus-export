c34e3af2f89cb930e2c31353dc9b2f87c325df48
csae8092 <p.andorfer@gmail.com>
2024-10-18 10:06:15 +0200

    better metadata

--- postprocess.py
+++ postprocess.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import glob
+import requests
 import lxml.etree as ET
 
 from jinja2 import Environment, FileSystemLoader
@@ -15,6 +16,11 @@ editions = os.path.join("data", "editions")
 shutil.rmtree(editions, ignore_errors=True)
 os.makedirs(editions, exist_ok=True)
 
+r = requests.get("https://raw.githubusercontent.com/TillichCorrespondence/tillich-entities/refs/heads/main/json_dumps/lectures_pages.json").json()
+lookup = {}
+for key, value in r.items():
+    if value["name"]:
+        lookup[value["tillich_id"]] = value
 
 col_id = "271480"
 files = sorted(glob.glob("./tei/*.xml"))
@@ -42,8 +48,6 @@ for i, x in enumerate(tqdm(files)):
         pb_node = doc.any_xpath(f'.//tei:pb[@facs="#{facs_id}"]')[0]
         xpath_expr = f'.//tei:ab[@facs="{facs_id}" or starts-with(@facs, "#{facs_id}_")] | .//tei:p[@facs="{facs_id}" or starts-with(@facs, "#{facs_id}_")]'
         ab_node = doc.any_xpath(xpath_expr)
-        if len(ab_node) > 1:
-            print(len(ab_node))
         ab_text = ""
         for abnode in ab_node:
             ab_text += (
@@ -82,6 +86,7 @@ for x in tqdm(files):
     doc.tree_to_file(x)
 
 
+print("renaming files")
 files = sorted(glob.glob('./data/editions/*xml'))
 for i, x in enumerate(files, start=1):
     doc = TeiReader(x)
@@ -95,17 +100,41 @@ for i, x in enumerate(files, start=1):
     doc.tree_to_file(os.path.join("data", "editions", f_name))
     os.remove(x)
 
+print("keyword-elements to rs-elements")
 files = sorted(glob.glob('./data/editions/*xml'))
-for x in files:
+for x in tqdm(files, total=len(files)):
     doc = TeiReader(x)
     body = doc.any_xpath(".//tei:body")[0]
     for element in body.iter():
         el_name = element.tag.replace("{http://www.tei-c.org/ns/1.0}", "")
         if el_name[0].isupper():
-            print(el_name)
             element.tag = "{http://www.tei-c.org/ns/1.0}rs"
             element.attrib["type"] = "keyword"
             element.attrib["ref"] = f"#{el_name}"
         elif element.tag == "{http://www.tei-c.org/ns/1.0}ab":
             element.tag = "{http://www.tei-c.org/ns/1.0}p"
+    doc.tree_to_file(x)
+
+print("adding metadata from baserow")
+for x in tqdm(files, total=len(files)):
+    f_name = os.path.split(x)[-1]
+    cur_nr = f_name.replace("TL-", "").replace(".xml", "")
+    doc = TeiReader(x)
+    try:
+        match = lookup[f_name]
+    except KeyError:
+        os.remove(x)
+        print(f"removing {x} because no metadata is provided")
+        continue
+    title = doc.any_xpath(".//tei:title[@type='main']")[0]
+    new_title = f'{match["name"]} (Nr. {cur_nr})'
+    title.text = new_title
+    if match["first_page"]:
+        title.attrib["subtype"] = "first_page"
+    if match["date"]:
+        date = doc.any_xpath(".//tei:setting/tei:date")[0]
+        date.attrib["when-iso"] = match["date"]
+        date.text = match["date"]
+    date = doc.any_xpath(".//tei:date[@type='term']")[0]
+    date.text = f'Semester {match["semester"]}'
     doc.tree_to_file(x)
