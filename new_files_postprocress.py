284b85a196bd2ff17e25708db126f7e3eaf43d9b
csae8092 <p.andorfer@gmail.com>
2026-04-15 12:25:42 +0200

    wip: making progress; ToDo: provide better tei:title

--- new_files_postprocress.py
+++ new_files_postprocress.py
@@ -7,9 +7,9 @@ from acdh_tei_pyutils.tei import TeiReader
 from acdh_xml_pyutils.xml import NSMAP as nsmap
 
 arche_base = "https://id.acdh.oeaw.ac.at/tillich-lectures/"
-editions = os.path.join("data", "editions")
-shutil.rmtree(editions, ignore_errors=True)
-os.makedirs(editions, exist_ok=True)
+new_items = os.path.join("data", "new_items")
+shutil.rmtree(new_items, ignore_errors=True)
+os.makedirs(new_items, exist_ok=True)
 
 col_id = "1956124"
 files = sorted(glob.glob("./tei/7*.xml"))
@@ -18,16 +18,18 @@ for part, x in enumerate(files, start=1):
     mets_name = f'{os.path.join("mets", col_id, tei_name.replace(".xml", "_image_name.xml"))}'
     mets = TeiReader(mets_name)
     img_lookup = [x.text for x in mets.any_xpath(".//item")]
+    new_name = f"TLx-{part:01}.xml"
     doc = TeiReader(x)
     for i, y in enumerate(doc.any_xpath(".//tei:pb")):
-        y.attrib["n"] = f"part{part:01}_{img_lookup[i]}"
+        y.attrib["n"] = f"TLx-{part:01}_{img_lookup[i]}"
     for i, y in enumerate(doc.any_xpath(".//tei:surface[@xml:id]")):
         graphic = y.xpath(".//tei:graphic", namespaces=nsmap)[0]
-        graphic.attrib["url"] = f"part{part:01}_{img_lookup[i]}"
-    doc.tree_to_file(x)
+        graphic.attrib["url"] = f"TLx-{part:01}_{img_lookup[i]}"
+    save_path = os.path.join(new_items, new_name)
+    doc.tree_to_file(save_path)
 
 
-files = glob.glob('./data/editions/*xml')
+files = glob.glob('./data/new_items/*.xml')
 print("fixing facs")
 for x in tqdm(files):
     doc = TeiReader(x)
@@ -38,7 +40,7 @@ for x in tqdm(files):
 
 
 print("renaming files")
-files = sorted(glob.glob('./data/editions/*xml'))
+files = sorted(glob.glob('./data/new_items/*.xml'))
 for i, x in enumerate(files, start=1):
     doc = TeiReader(x)
     cur_id = f"TL-{i:04}"
@@ -48,11 +50,11 @@ for i, x in enumerate(files, start=1):
 
     pb = doc.any_xpath(".//tei:pb")[0]
     pb.attrib["facs"] = f"#{cur_id}.jpg"
-    doc.tree_to_file(os.path.join("data", "editions", f_name))
+    doc.tree_to_file(os.path.join("data", "new_items", f_name))
     os.remove(x)
 
 print("keyword-elements to rs-elements")
-files = sorted(glob.glob('./data/editions/*xml'))
+files = sorted(glob.glob('./data/new_items/*.xml'))
 for x in tqdm(files, total=len(files)):
     doc = TeiReader(x)
     body = doc.any_xpath(".//tei:body")[0]
